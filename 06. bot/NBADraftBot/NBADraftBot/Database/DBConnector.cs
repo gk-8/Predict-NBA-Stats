cc2ad9a4ca4e94a9203dc83d48e1284749a51df3
Gorka Madariaga Nuñez <gorkma@microsoft.com>
2016-10-28 15:55:07 +0200

    Bot code added

---
+++ 06. bot/NBADraftBot/NBADraftBot/Database/DBConnector.cs
@@ -0,0 +1,55 @@
+﻿using NBADraftBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NBADraftBot.Database
+{
+    public class DBConnector
+    {
+        private NBABotContext db;
+
+        public DBConnector()
+        {
+            db = new NBABotContext();
+        }
+
+        public List<Predictions> GetAvailablePlayersInfoPerDescending()
+        {
+            return db.PlayersInfo.Where(p => p.Drafted == 0).OrderByDescending(p => p.PER).ToList();
+        }
+
+        public List<Predictions> GetAvailableTopPlayersInfoPerDescending(int number)
+        {
+            return db.PlayersInfo.Where(p => p.Drafted == 0).OrderByDescending(p => p.PER).Take(number).ToList();
+        }
+
+        public List<Predictions> GetAvailableTopPlayersByPosition(string position, int number)
+        {
+            return db.PlayersInfo.Where(p => p.Position == position && p.Drafted == 0).OrderByDescending(p => p.PER).Take(number).ToList();
+        }
+
+        public List<Predictions> GetAvailablePlayersByPosition(string position)
+        {
+            return db.PlayersInfo.Where(p => p.Position == position && p.Drafted == 0).OrderByDescending(p => p.PER).ToList();
+        }
+
+        public Predictions GetPlayerInfoByName(string player)
+        {
+            return db.PlayersInfo.Where(p => p.Player == player).FirstOrDefault();
+        }
+
+        public bool CheckPlayerAvailability(string player) {
+            return GetPlayerInfoByName(player).Drafted == 0;
+        }
+
+        public void MarkPlayerAsDraftedByName(string player) {
+            Predictions p = GetPlayerInfoByName(player);
+            if (p != null) {
+                p.Drafted = 1;
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ 06. bot/NBADraftBot/NBADraftBot/Models/PlayerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace NBADraftBot.Models
+{
+
+    public class Predictions
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Player { get; set; }
+        public string Position { get; set; }
+        public int? Age { get; set; }
+        public string Team { get; set; }
+        public int? Season { get; set; }
+        public double? PER { get; set; }
+        public int? Drafted { get; set; }
+    }
+}
